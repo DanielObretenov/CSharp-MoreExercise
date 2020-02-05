39e841f69a41a07c74e73a03159fbf8fe1ac18d2
Daniel Obretenov <daniel.hr.obretenov@gmail.com>
2020-02-05 16:41:13 +0200

    Define a class Student, which holds the following information about students: name, list of grades and average grade (calculated property, read-only). A single grade will be in range [2…6], e.g. 3.25 or 5.50. Read a list of students and print the students that have average grade ≥ 5.00 ordered by name (ascending), then by average grade (descending). Print the student name and the calculated average grade.

---
+++ OOP-Exercises/OOP-Exercises/Exercise 4/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP_Exercises.Exercise_4
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public double [] Grades { get; set; }
+        public double AverageGrade { get; set; }
+
+        public Student(string name, double[] grades)
+        {
+            Name = name;
+            Grades = grades;
+            AverageGrade = grades.Average();
+        }
+    }
+}
--- OOP-Exercises/OOP-Exercises/Program.cs
+++ OOP-Exercises/OOP-Exercises/Program.cs
@@ -1,6 +1,8 @@
 ﻿using OOP_Exercises.Exercise_1;
 using OOP_Exercises.Exercise_3;
+using OOP_Exercises.Exercise_4;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
@@ -10,15 +12,34 @@ namespace OOP_Exercises
     {
         static void Main(string[] args)
         {
-            //Exercise 1 
+            // Exercise 1 - Get Working days for a period of time
+            // Expected Input - 11-04-2016 /r  14-04-2016
+            // Expected Output - (Number of days)
+            ExerciseOne();
+
+            // Exercise 2 - Intersecting Circles ( True - False )
+            // Expected Input - 4 4 2 / r  8 8 1
+            // Expected Output - (Yes - No)
+            ExerciseTwo();
+
+            // Exercise 3 - Get Average Student Grade - 
+            // {Number of Students} /r {Name} {Grade} {Grade}..
+            ExerciseThree();
+        }
+
+        public static void ExerciseOne()
+        {
+           
             string startDate = Console.ReadLine();
             string endDate = Console.ReadLine();
             ReadDate readDate = new ReadDate();
-            DatePeriod datePeriod = new DatePeriod(readDate.Read(startDate), readDate.Read(endDate));
+            DatePeriod datePeriod = new DatePeriod(Read(startDate), Read(endDate));
             Console.WriteLine(datePeriod.CalculateWorkingDays(datePeriod));
+        }
 
-
-            // Exercise 3 
+        public static void ExerciseTwo()
+        {
+          
             double[] firstInput = Console.ReadLine().Split().Select(double.Parse).ToArray();
             double[] secondInput = Console.ReadLine().Split().Select(double.Parse).ToArray();
 
@@ -27,6 +48,55 @@ namespace OOP_Exercises
             string areIntersected = first.AreIntersecting(first, second) ? "Yes" : "No";
             Console.WriteLine(areIntersected);
         }
+        public static void ExerciseThree()
+        {
+
+            int numberStudents = int.Parse(Console.ReadLine());
+            List<Student> allStudents = ReadStudent(numberStudents);
+            allStudents.ForEach(
+                student => Console.WriteLine($"{student.Name} -> {Math.Round(student.AverageGrade, 2)}"));
+        }
+
+        //Exercise 1 - Read Date
+        public static DateTime Read(string date)
+        {
+            string format = "dd-MM-yyyy";
+            DateTime dateTime = DateTime.ParseExact(date, format,
+                                           CultureInfo.InvariantCulture);
+            return dateTime;
+        }
+
+        //Exercise 4 - Read Students
+        public static List<Student> ReadStudent(int numberStudents)
+        {
+
+            List<Student> allStudents = new List<Student>();
+
+            int count = 1;
+
+            while (count <= numberStudents)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                string name = input[0];
+                double[] grades = input.Select(s => double.Parse(s)).Skip(1).ToArray();
+                for (int i = 0; i < grades.Length; i++)
+                {
+                    if (grades[i] >= 2 && grades[i] <= 6)
+                    {
+                        allStudents.Add(new Student(name, grades));
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Please enter valid grade in order to get average grades -> {input} ");
+                    }
+                }
+                count++;
+            }
+            return allStudents;
+        }
+      
 
     }
+
 }
